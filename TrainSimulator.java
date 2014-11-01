9a4a8ddbc10d6f71ccda20705fe7d06acbeb39b9
Andrew Hard <hardandrew1@gmail.com>
2014-11-01 18:06:24 -0500

    Commenting, simplifying TrainSimulator

--- TrainSimulator.java
+++ TrainSimulator.java
@@ -26,12 +26,15 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-/*
+/**
+ * The TrainSimulator class simulates a series of trains moving from station
+ * to station in order to calculate the ATA and ATD of the trains from each
+ * station in the simulation. Station capacity data and train ETD data are
+ * stored in separate input files to be read in the main method.
  */
 
 public class TrainSimulator{
@@ -42,6 +45,8 @@ public class TrainSimulator{
      * actual time of arrival (ATA) and the actual time of departure (ATD)
      * of each train for each station.
      */
+    
+    // Question: shouldn't the Platform, Queue, and Stack exceptions be caught in the program?
     public static void main(String[] args) throws FileNotFoundException,
     FullPlatformException, EmptyPlatformException, FullQueueException, EmptyQueueException {
 	
@@ -52,22 +57,28 @@ public class TrainSimulator{
 	    System.exit(0);
 	}
 	
-	// Assign value to the args[0] integer
+	// Value of args[0] integer determines the print command:
 	int infoCommand = Integer.parseInt(args[0]);
-	
 	if (infoCommand < 0 || infoCommand > 2) {
 	    throw new IllegalArgumentException();
 	}
+	// Assign booleans based on the input command:
+	boolean print = (infoCommand == 0);
+	boolean printATD = (infoCommand == 1);
+	boolean printATA = (infoCommand == 2);
 	
-	
+
 	// Data storage variables:
 	int nTrains = 0;// number of trains in simulation
 	int nStations = 0;// number of stations in simulation
-	List<Train> allTrains = new ArrayList<Train>();// Store all Trains
-	List<Station> allStations = new ArrayList<Station>();// Store all Stations
+	List<Train> allTrains = new ArrayList<Train>();// Store Trains
+	List<Station> allStations = new ArrayList<Station>();// Store Stations
+	List<SimpleQueue<Train>> allTracks
+	    = new ArrayList<SimpleQueue<Train>>();// Store tracks
+	SimpleQueue<Train> track;
 	
 	
-	// Load file for station data
+	// Load file for station data:
 	File stationFile = new File(args[1]);
 	if (!stationFile.exists()) {
 	    throw new FileNotFoundException();
@@ -79,13 +90,13 @@ public class TrainSimulator{
 	    
 	    String currLine = stationInput.nextLine();
 	    
-	    // first line gives the number of stations:
+	    // First line gives the number of stations:
 	    if (is1stLine) {
 		is1stLine = false;
-		// read the number of stations:
 		nStations = Integer.parseInt(currLine);
 	    }
-	    // following lines give the station ID and capacity:
+	    
+	    // The remaning lines give the station ID and capacity:
 	    else {
 		String delims = ",";
 		String[] splitLine = currLine.split(delims);
@@ -95,10 +106,10 @@ public class TrainSimulator{
 					    Integer.parseInt(splitLine[1])));
 	    }
 	}
-	stationInput.close();// finished loading station data
+	stationInput.close();// Finished loading station data
 	
 	
-	// Load file 2 for train data
+	// Load a new file for train data:
 	File trainFile = new File(args[2]);
 	if (!trainFile.exists()) {
 	    throw new FileNotFoundException();
@@ -110,13 +121,13 @@ public class TrainSimulator{
 	    
 	    String currLine = trainInput.nextLine();
 	    
-	    // first line gives the number of trains:
+	    // First line gives the number of trains:
 	    if (is1stLine) {
 		is1stLine = false;
 		// read the number of trains:
 		nTrains = Integer.parseInt(currLine);
 	    }
-	    // following lines give the train ID and ETDs:
+	    // The remaining lines give the train IDs and ETDs:
 	    else {
 		String delims = ",";
 		String[] splitLine = currLine.split(delims);
@@ -129,151 +140,147 @@ public class TrainSimulator{
 		}
 		allTrains.add(currTrain);
 	    }
-	    
 	}
-	trainInput.close();// finished loading train data
-
-	boolean print = false;
-	boolean printATD = false;
-	boolean printATA = false;
-
-		switch(infoCommand){
-		case 0:
-			print = true;
-		break;
-		case 1:
-			printATD = true;
-		break;
-		case 2:
-			printATA = true;
-		break;
-		default:
-		System.out.println("Error");
-		break;
-	}//user command
+	trainInput.close();// Finished loading train data
 	
-	//Train movement
+	
+	// Initialize allTracks (having FIFO structures):
+	for (int i = 0; i < nStations-1; i++) {
+	    // Each track has capacity to carry all simulated trains:
+	    track = new SimpleQueue<Train>(nTrains);
+	    allTracks.add(track);
+	}
+	
+	
+	// Train movement variables:
 	int time = 0;
 	boolean movement = true;
 	boolean done = false;
 	
-	//Initialize train tracks(nStations -1), time = 0
-	List<SimpleQueue<Train>> allTracks = new ArrayList<SimpleQueue<Train>>();
-	SimpleQueue<Train> track;
+	// Variables to be assigned as needed:
 	Train train;
 	Platform currPlatform;
 	
-	
-	for(int i = 0; i < nStations-1; i++){
-		track = new SimpleQueue<Train>(100);
-		allTracks.add(track);
+	// Set up the simulation at time = 0 by adding trains 
+	// to 1st station platform:
+	for (int i = nTrains-1; i >= 0; i--) {
+	    allStations.get(0).getPlatform().put(allTrains.get(i));
+	    allTrains.get(i).getATD().add(0);//set actual departure time = 0
 	}
+	
+	
 
-	//set up the simulation, time = 0
-	for(int i = nTrains-1; i >= 0; i--){
-		allStations.get(0).getPlatform().put(allTrains.get(i));
-		allTrains.get(i).getATD().add(0);//set departure time to 0
-	}//add trains to platform of first station
-
-	//movement begins
-	while(movement){
-		for(int i = 0; i < nStations-1; i++){
-			currPlatform = allStations.get(i).getPlatform();//get platform
-
-			done = false;
-			track = allTracks.get(i);//get tracks
-
-			while(!done){
-				if(!currPlatform.isEmpty()){
-				train = currPlatform.check();//get train
-				
-				if(train.getETD().get(i) <= time && train.getATD().get(i) <= time){
-					track.enqueue(currPlatform.get());
-					if(print){
-					System.out.printf("%d:\tTrain %d has exited from station %d.\n",time,train.getId(),i+1);
-				}
-					train.getATD().remove(i);
-					train.getATD().add(time);//set actual departure time
-					train.getATA().add(time+10);//set arrival time
-				}//check ATD and ETD to see whether the train can depart
-				else{
-					done = true;
-				}
-				}
-				else{
-					done = true;
-				}
-			}//check to see whether train can depart in that station
-		}//from platform to tracks
-
-		for(int i=0; i< nStations-1; i++){
-			currPlatform = allStations.get(i+1).getPlatform();//get platform
+	// Begin simulation of movement:
+	while (movement) {
+	    
+	    // Loop over stations, move from platform to tracks:
+	    for (int i = 0; i < nStations-1; i++) {
+		currPlatform = allStations.get(i).getPlatform();//get platform
+		
+		done = false;
+		// The ith track follows the ith station:
+		track = allTracks.get(i);//get tracks
+		
+		// Check to see whether train can depart in that station:
+		while (!done) {
+		    if (!currPlatform.isEmpty()) {
+			train = currPlatform.check();//get train
 			
-			track = allTracks.get(i);//get track
-			done = false;
-			while(!done){
-				if(!track.isEmpty() && !currPlatform.isFull()){
-				train = track.peek();//get train
-				if(train.getATA().get(i) <= time){
-					currPlatform.put(track.dequeue());
-				if(print){
-					System.out.printf("%d:\tTrain %d has been parked at station %d.\n",time,train.getId(),i+2);
-				}
-					train.getATA().remove(i);
-					train.getATA().add(i,time);//set actual time of arrival
-					train.getATD().add(time+1);//stay at platfrom for at least 1s
-				}//check ATA to see whether train can arrive at platform
-				else{
-					done = true;
-				}
-				}
-				else{
-					done = true;
-				}
+			//check ATD and ETD to see whether the train can depart
+			if (train.getETD().get(i) <= time 
+			    && train.getATD().get(i) <= time) {
+			    
+			    track.enqueue(currPlatform.get());
+			    if (print) {
+				System.out.printf(
+"%d:\tTrain %d has exited from station %d.\n",time,train.getId(),i+1);
+			    }
+			    train.getATD().remove(i);
+			    train.getATD().add(time);//set ATD
+			    train.getATA().add(time+10);//set ATA
 			}
+			else {
+			    done = true;
+			}
+		    }
+		    else {
+			done = true;
+		    }
+		}// end while loop
+	    }//end 1st loop over stations
+	    
+	    // Loop over stations again, move from tracks to platforms:
+	    for (int i = 0; i < nStations-1; i++) {
+		
+		currPlatform = allStations.get(i+1).getPlatform();//get platform
+		
+		track = allTracks.get(i);//get track
+		done = false;
+	
+		while (!done) {
+		
+		    if (!track.isEmpty() && !currPlatform.isFull()) {
+			train = track.peek();//get train
 			
+			//check ATA to see whether train can arrive at platform:
+			if (train.getATA().get(i) <= time) {
+			    currPlatform.put(track.dequeue());
+			    
+			    if (print) {
+				System.out.printf(
+"%d:\tTrain %d has been parked at station %d.\n",time,train.getId(),i+2);
+			    }
+			    train.getATA().remove(i);
+			    train.getATA().add(i,time);//set ATA
+			    //stay at platfrom for at least +1s:
+			    train.getATD().add(time+1);
+			}
+			else {
+			    done = true;
+			}
+		    }
+		    else {
+			done = true;
+		    }
 		}
 		
-		if(allStations.get(nStations-1).getPlatform().isFull()){
-			movement = false;
-		}//check whehter last platform is full
+	    }// end 2nd loop over stations
+	    
+	    // Question: Is the last station guaranteed to have the same capacity as the number of trains? What if there are fewer trains?
+	    if (allStations.get(nStations-1).getPlatform().isFull()) {
+		movement = false;
+	    }//check whehter last platform is full
+	    
+	    // Increment the time each iteration.
+	    time++;
 
-		time ++;
-	}
-
-	List<Integer> temp;
-
-	if(printATD){
-		for(int i = 0; i < nTrains; i++){
-			temp = allTrains.get(i).getATD();
-
-			System.out.print("[");
-			for(int j = 0; j < nStations-1; j++){
-				if(j == nStations-2){
-					System.out.println(temp.get(j) + "]");
-				}
-				else{
-					System.out.print(temp.get(j) + ", ");
-				}
-			}
-		}		
-	}
-
-	if(printATA){
-		for(int i = 0; i < nTrains; i++){
-			temp = allTrains.get(i).getATA();
-
-			System.out.print("[");
-			for(int j = 0; j < nStations-1; j++){
-				if(j == nStations-2){
-					System.out.println(temp.get(j) + "]");
-				}
-				else{
-					System.out.print(temp.get(j) + ", ");
-				}
-			}
-		}	
+	}// End of simulation
+	
+	
+	
+	List<Integer> tempList;// Store ATA, ATD info temporarily
+	
+	// Lastly, loop over trains to print ATA/ATD info as asked:
+	if (printATD || printATA) {
+	    for (int i = 0; i < nTrains; i++) {
+		
+		// Option 1: Print out actual departure times:
+		if (printATD) { tempList = allTrains.get(i).getATD(); }
+	
+		// Option 2: Print out actual arrival times:
+		else { tempList = allTrains.get(i).getATA(); }//printATA
+		
+		System.out.print("[");
+		for (int j = 0; j < nStations-1; j++) {
+		    if (j == nStations-2) {
+			System.out.println(tempList.get(j) + "]");
+		    }
+		    else {
+			System.out.print(tempList.get(j) + ", ");
+		    }
+		}
+	    }
+	}// end printATD and printATA sections.
 	
     }
-}
 }
